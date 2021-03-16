c8cee708c2d09bfe197e0cd2e23432a0829b50c1
Abhaysardhara <abhays7675@gmail.com>
2021-03-16 16:40:55 +0530

    service worker

--- views/sw.js
+++ views/sw.js
@@ -11,6 +11,13 @@ var isValid = function (response) {
 	return false;
 };
 
+var isValidFront = function (response) {
+	if (!response) return false;
+	var fetched = response.headers.get('sw-fetched-on');
+	if (fetched && (parseFloat(fetched) + (300 * 1000)) > new Date().getTime()) return true;
+	return false;
+};
+
 self.addEventListener('fetch', function(event) {
   const {request} = event;
 
@@ -38,21 +45,28 @@ self.addEventListener('fetch', function(event) {
       })
     );
   }
+  else if(request.url.includes('/') || request.url.includes('/india') || request.url.includes('/chart')) {
+    event.respondWith(
+      caches.match(request).then(response => {
+        if(isValidFront(response)) {
+          return response;
+        }
+        return fetch(request).then(response => {
+          var copy = response.clone();
+          event.waitUntil(caches.open('covidFiles').then(cache => {
+            var headers = new Headers(copy.headers);
+            headers.append('sw-fetched-on', new Date().getTime());
+            return copy.blob().then(function (body) {
+              return cache.put(request, new Response(body, {
+                status: copy.status,
+							  statusText: copy.statusText,
+                headers: headers
+              }));
+            });
+          }));
+          return response;
+        });
+      })
+    );
+  }
 });
-
-// async function updateAPI() {
-//   const articlesCache = await caches.open('covidFiles');
-//   await articlesCache.addAll([
-//     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
-//     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv',
-//     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv',
-//     'https://api.covid19india.org/v4/min/timeseries.min.json'
-//   ]);
-// }
-
-// self.addEventListener('periodicsync', (event) => {
-//   if (event.tag === 'syncAPI') {
-//     event.waitUntil(updateAPI());
-//   }
-//   console.log('updated');
-// });
