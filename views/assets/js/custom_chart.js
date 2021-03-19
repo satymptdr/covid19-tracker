6a174440d4a394124cf714d7d067b437120bfc85
Abhaysardhara <abhays7675@gmail.com>
2021-03-19 15:37:32 +0530

    Multiple chart

--- views/assets/js/custom_chart.js
+++ views/assets/js/custom_chart.js
@@ -6,6 +6,8 @@ var config,
     ydata2 = [],
     ydata3 = [],
     ydata4 = [],
+    ydata5 = [],
+    ydata6 = [],
     india_time = [],
     state_name = "GJ",
     color = Chart.helpers.color,
@@ -13,9 +15,14 @@ var config,
     bg = color(window.chartColors.blue).alpha(0.5).rgbString(),
     border = window.chartColors.blue;
 
-// var push_confirm = { id: "confirm_cases", label: "Confirmed Cases", backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(), borderColor: window.chartColors.blue, pointRadius: 1, fill: !1, data: ydata1 };
-// var push_recovered = { id: "recovered_cases", label: "Recovered Cases", backgroundColor: color(window.chartColors.yellow).alpha(0.5).rgbString(), borderColor: window.chartColors.yellow, pointRadius: 1, fill: !1, data: ydata2 };
-// var push_death = { id: "death", label: "Deaths Cases", backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(), borderColor: window.chartColors.red, pointRadius: 1, fill: !1, data: ydata3 };
+var state_obj = [
+    { id: "confirm_cases", label: "Confirm", backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(), borderColor: window.chartColors.red, data: null},
+    { id: "recovered_cases", label: "Recovered", backgroundColor: color(window.chartColors.green).alpha(0.5).rgbString(), borderColor: window.chartColors.green, data: null },
+    { id: "active", label: "Active", backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(), borderColor: window.chartColors.blue, data: null },
+    { id: "death", label: "Deceased", backgroundColor: color(window.chartColors.grey).alpha(0.5).rgbString(), borderColor: window.chartColors.grey, data: null },
+    { id: "tested", label: "Tested", backgroundColor: color(window.chartColors.purple).alpha(0.5).rgbString(), borderColor: window.chartColors.purple, data: null },
+    { id: "vaccinated", label: "Vaccinated", backgroundColor: color(window.chartColors.yellow).alpha(0.5).rgbString(), borderColor: window.chartColors.yellow, data: null }
+];
 var temp = { label: series_label, backgroundColor: bg, borderColor: border, pointRadius: 1, fill: !0, data: india_time };
 
 async function getData() {
@@ -29,19 +36,33 @@ async function getData() {
     ydata2.length = 0;
     ydata3.length = 0;
     ydata4.length = 0;
+    ydata5.length = 0;
+    ydata6.length = 0;
 
-    var a,b,c;
+    var a,b,c,d,e;
     Object.keys(data[state_name]).map(i => {
         xlables.forEach(ele => {
             a = data[state_name][i][ele]['total']['confirmed'] || !1;
             b = data[state_name][i][ele]['total']['recovered'] || !1;
             c = data[state_name][i][ele]['total']['deceased'] || !1;
+            d = data[state_name][i][ele]['total']['tested'] || !1;
+            e = data[state_name][i][ele]['total']['vaccinated'] || !1;
+            
             ydata1.push(a);
             ydata2.push(b);
             ydata3.push(c);
+            ydata5.push(d);
+            ydata6.push(e);
             ydata4.push(a-b-c);
         });
     })
+
+    state_obj[0].data = ydata1;
+    state_obj[1].data = ydata2;
+    state_obj[2].data = ydata4;
+    state_obj[3].data = ydata3;
+    state_obj[4].data = ydata5;
+    state_obj[5].data = ydata6;
 }
 async function timeSeries(e = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv") {
     var a = await fetch(e);
@@ -63,10 +84,7 @@ function getChart() {
         data: {
             labels: xlables,
             datasets: [
-                { id: "confirm_cases", label: "Confirm", backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(), borderColor: window.chartColors.red, pointRadius: 1, fill: !1, data: ydata1 },
-                { id: "recovered_cases", label: "Recovered", backgroundColor: color(window.chartColors.green).alpha(0.5).rgbString(), borderColor: window.chartColors.green, pointRadius: 1, fill: !1, data: ydata2 },
-                { id: "death", label: "Deceased", backgroundColor: color(window.chartColors.grey).alpha(0.5).rgbString(), borderColor: window.chartColors.grey, pointRadius: 1, fill: !1, data: ydata3 },
-                { id: "active", label: "Active", backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(), borderColor: window.chartColors.blue, pointRadius: 1, fill: !1, data: ydata4 },
+                { label: "Confirm", backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(), borderColor: window.chartColors.red, pointRadius: 1, fill: !1, data: ydata1 }
             ],
         },
         options: {
@@ -139,15 +157,17 @@ function select_operation() {
     ydata2.length = 0;
     ydata3.length = 0;
     ydata4.length = 0;
+    ydata5.length = 0;
+    ydata6.length = 0;
 	
     state_name = document.getElementById('states').value;
 
     getData().then(() => {
         window.myLine.data.labels = xlables;
         window.myLine.data.datasets[0].data = ydata1;
-        window.myLine.data.datasets[1].data = ydata2;
-        window.myLine.data.datasets[2].data = ydata3;
-        window.myLine.data.datasets[3].data = ydata4;
+        window.myLine.data.datasets[0].label = state_obj[0].label;
+        window.myLine.data.datasets[0].backgroundColor = state_obj[0].backgroundColor;
+        window.myLine.data.datasets[0].borderColor = state_obj[0].borderColor;
         window.myLine.update();
     });
 }
@@ -176,139 +196,24 @@ download_img1 = function (e) {
     e.href = a;
 };
 
-// document.getElementById('show_confirm_cases').addEventListener('click', function() {
-//     let available = false;
-//     for(var i = 0; i < config.data.datasets.length; i++) {
-//         if(config.data.datasets[i].id == 'confirm_cases') {
-//             available = true;
-//         }
-//     }
-    
-//     if(available == false) {
-//         config.data.datasets.push(push_confirm);
+function changeData(index) {
+    window.myLine.data.datasets.forEach(function(dataset) {
+        dataset.label = state_obj[index].label;
+        dataset.data = state_obj[index].data;
+        dataset.backgroundColor = state_obj[index].backgroundColor;
+        dataset.borderColor = state_obj[index].borderColor;
+    });
+    window.myLine.update();
+}
 
-//         if(config.data.datasets.length == 1) {
-//             config.data.datasets[0].fill = true;
-//         }
-
-//         if(config.data.datasets.length > 1) {
-//             for(var i=0; i<config.data.datasets.length; i++) {
-//                 config.data.datasets[i].fill = false;
-//             }
-//         }
-
-//         window.myLine.update();
-//         document.getElementById('hide_confirm_cases').disabled = false;
-//         document.getElementById('show_confirm_cases').disabled = true;
-//     }
-// });
-
-// document.getElementById('show_recovered_cases').addEventListener('click', function() {
-//     var available = false;
-//     for(var i = 0; i < config.data.datasets.length; i++) {
-//         if(config.data.datasets[i].id == 'recovered_cases') {
-//             available = true;
-//         }
-//     }
-    
-//     if(available == false) {
-//         config.data.datasets.push(push_recovered);
-
-//         if(config.data.datasets.length == 1) {
-//             config.data.datasets[0].fill = true;
-//         }
-
-//         if(config.data.datasets.length > 1) {
-//             for(var i=0; i<config.data.datasets.length; i++) {
-//                 config.data.datasets[i].fill = false;
-//             }
-//         }
-
-//         window.myLine.update();
-//         document.getElementById('hide_recovered_cases').disabled = false;
-//         document.getElementById('show_recovered_cases').disabled = true;
-//     }
-    
-// });
-
-// document.getElementById('show_death').addEventListener('click', function() {
-//     var available = false;
-//     for(var i = 0; i < config.data.datasets.length; i++) {
-//         if(config.data.datasets[i].id == 'death') {
-//             available = true;
-//         }
-//     }
-    
-//     if(available == false) {
-//         config.data.datasets.push(push_death);
-
-//         if(config.data.datasets.length == 1) {
-//             config.data.datasets[0].fill = true;
-//         }
-
-//         if(config.data.datasets.length > 1) {
-//             for(var i=0; i<config.data.datasets.length; i++) {
-//                 config.data.datasets[i].fill = false;
-//             }
-//         }
-
-//         window.myLine.update();
-//         document.getElementById('hide_death').disabled = false;
-//         document.getElementById('show_death').disabled = true;
-//     } 
-// });
-
-// document.getElementById('hide_confirm_cases').addEventListener('click', function() {
-//     var i;
-//     for(i = 0; i < config.data.datasets.length; i++) {
-//         if(config.data.datasets[i].id == 'confirm_cases') {
-//             break;
-//         }
-//     }
-//     config.data.datasets.splice(i, 1);
-
-//     if(config.data.datasets.length == 1) {
-//         config.data.datasets[0].fill = true;
-//     }
-
-//     window.myLine.update();
-//     document.getElementById('hide_confirm_cases').disabled = true;
-//     document.getElementById('show_confirm_cases').disabled = false;
-// });
-
-// document.getElementById('hide_recovered_cases').addEventListener('click', function() {
-//     var i;
-//     for(i = 0; i < config.data.datasets.length; i++) {
-//         if(config.data.datasets[i].id == 'recovered_cases') {
-//             break;
-//         }
-//     }
-    
-//     config.data.datasets.splice(i, 1);
-
-//     if(config.data.datasets.length == 1) {
-//         config.data.datasets[0].fill = true;
-//     }
-
-//     window.myLine.update();
-//     document.getElementById('hide_recovered_cases').disabled = true;
-//     document.getElementById('show_recovered_cases').disabled = false;
-// });
-
-// document.getElementById('hide_death').addEventListener('click', function() {
-//     var i;
-//     for(i = 0; i < config.data.datasets.length; i++) {
-//         if(config.data.datasets[i].id == 'death') {
-//             break;
-//         }
-//     }
-//     config.data.datasets.splice(i, 1);
-
-//     if(config.data.datasets.length == 1) {
-//         config.data.datasets[0].fill = true;
-//     }
-
-//     window.myLine.update();
-//     document.getElementById('hide_death').disabled = true;
-//     document.getElementById('show_death').disabled = false;
-// });
+/* add active class on click */
+// Add active class to the current button (highlight it)
+// var header = document.getElementById("myDIV");
+// var btns = header.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }   
